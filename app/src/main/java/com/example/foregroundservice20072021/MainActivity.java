5378040c7b33e03e71e1b0987301d618a3fcb667
phamtanphat <phatdroid94@gmail.com>
2021-10-12 18:23:07 +0700

    khai bao manifest

--- app/src/main/java/com/example/foregroundservice20072021/MainActivity.java
+++ app/src/main/java/com/example/foregroundservice20072021/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.foregroundservice20072021;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button mBtnStartService,mBtnStopService;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtnStartService = findViewById(R.id.buttonStartForeground);
+        mBtnStopService = findViewById(R.id.buttonStopForeground);
+
+        mBtnStartService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
